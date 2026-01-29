08b4cdeac5413494b73ea97b401b16b9dcf1bf7f
Kevanojb <132914277+Kevanojb@users.noreply.github.com>
2026-01-29 14:16:55 +0000

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/den-society-vite/",
+  base: "/golf/",
   plugins: [
     react(),
     VitePWA({
@@ -11,17 +11,22 @@ export default defineConfig({
       manifest: {
         name: "Den Society League",
         short_name: "Den League",
-        start_url: "/den-society-vite/index.html",
-        scope: "/den-society-vite/",
+        start_url: "/golf/index.html",
+        scope: "/golf/",
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#10b981",
         icons: [
           { src: "pwa-192.png", sizes: "192x192", type: "image/png" },
           { src: "pwa-512.png", sizes: "512x512", type: "image/png" },
-          { src: "pwa-512.png", sizes: "512x512", type: "image/png", purpose: "maskable any" }
-        ]
-      }
-    })
+          {
+            src: "pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+        ],
+      },
+    }),
   ],
 });
