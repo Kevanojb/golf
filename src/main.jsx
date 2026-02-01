3c6e5e2584626f7e10dccf465438dc65853ef094
Kevanojb <132914277+Kevanojb@users.noreply.github.com>
2026-02-01 13:03:45 +0000

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import Bootstrap from "./Bootstrap.jsx";
 import "./index.css";
@@ -8,8 +8,8 @@ import "./legacy.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter basename={import.meta.env.BASE_URL}>
       <Bootstrap />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
