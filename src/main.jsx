17fb2ae417d520c0bc40f759bdd28c5252c18f56
Kevanojb <132914277+Kevanojb@users.noreply.github.com>
2026-02-01 13:11:25 +0000

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-<script>
-  (function () {
-    var base = "/golf/";
-    var path = window.location.pathname;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-    // If someone hits /golf/<slug>, convert to /golf/#/<slug>
-    if (path.startsWith(base) && path !== base && path !== base + "index.html") {
-      var slug = path.slice(base.length); // everything after /golf/
-      window.location.replace(base + "#/" + slug + window.location.search + window.location.hash);
-    }
-  })();
-</script>
+import Bootstrap from "./Bootstrap.jsx";
+import "./index.css";
+import "./legacy.css";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Bootstrap />
+    </BrowserRouter>
+  </React.StrictMode>
+);
